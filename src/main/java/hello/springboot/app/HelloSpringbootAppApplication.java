fcaac48fcf617fbd254621508ccb026ca02d686e
zzyzzy <zzyzzy@gmail.com>
2025-01-06 20:37:47 +0900

    initial commit

---
+++ src/main/java/hello/springboot/app/HelloSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package hello.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springboot/app/HelloSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
